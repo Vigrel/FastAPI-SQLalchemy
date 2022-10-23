08771a78c368cce913df514e2ac7b419a85e5c0a
arthurchieppe <agchieppe@gmail.com>
2022-10-23 14:06:52 -0300

    Delete funcional

--- projeto.py
+++ projeto.py
@@ -2,9 +2,13 @@ from fastapi import FastAPI, Path, HTTPException
 from enum import Enum
 from pydantic import BaseModel
 from models import *
+import itertools
+
+counter = itertools.count()
+
 
 products = [
-    Product(id=0, name="Product 1", description="Description 1", price=10.0, quantity=10, tax=0.5),
+    Product(id=next(counter), name="Product 1", description="Description 1", price=10.0, quantity=10, tax=0.5),
 ]
 app = FastAPI()
 
@@ -13,15 +17,22 @@ app = FastAPI()
 async def get_products():
     return products
 
-# @app.get("/products/{product_id}")
-# async def get_product(product_id: int = Path(..., gt=0, le=len(products))):
+@app.delete("/products/{product_id}", status_code=204)
+async def get_product(product_id: int = Path(..., title="The ID of the item to delete")):
+    for product in products:
+        if product.id == product_id:
+            products.remove(product)
+            return
+    raise HTTPException(status_code=404, detail="Product not found")
+    
+
 
 @app.post("/products/", status_code=201)
-async def add_product(product: Product):
+async def add_product(product: Product, response_model=Product):
+    product.id = next(counter)
     product_dict = product.dict()
-    if product.name in products:
-        raise HTTPException(status_code=400, detail="Product already exists")
-    product_dict["id"] = len(products)
+    # if product.name in products:
+    #     raise HTTPException(status_code=400, detail="Product already exists")
     products.append(Product(**product_dict))
     return product
 
